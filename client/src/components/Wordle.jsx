b0bdd35192c3815301eecd3b8dd40cd462143a7a
Pratham Hebbar <prathamhebbar2021@gmail.com>
2026-05-02 12:07:13 -0700

    wordle component

--- client/src/components/Wordle.jsx
+++ client/src/components/Wordle.jsx
@@ -16,7 +16,6 @@ export default function Wordle({ solution }) {
   
   return (
     <div>
-      <div>current guess - {currentGuess}</div>
       <Grid currentGuess={currentGuess} guesses={guesses} turnCount={turnCount}/>
     </div>
   )
