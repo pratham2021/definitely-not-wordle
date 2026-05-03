dc6b6f3c8787666eea8c4fe59122e735e07e2932
Pratham Hebbar <prathamhebbar2021@gmail.com>
2026-05-02 20:50:13 -0700

    fix sendFile path

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.get('/ping-wordle', async (req, res) => {
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('*path', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
