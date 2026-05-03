1dcafd2b60a13fd6298b55c9163fb44f0771e3a1
Pratham Hebbar <prathamhebbar2021@gmail.com>
2026-05-02 20:21:31 -0700

    update server for production

--- server/server.js
+++ server/server.js
@@ -1,15 +1,7 @@
 const express = require('express');
-const cors = require("cors");
 const path = require("path");
 
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'client/dist')));
-
-app.use(cors({ 
-  origin: 'http://localhost:5173',
-  methods: ['GET'],
-}));
 
 app.use(express.json());
 
@@ -27,16 +19,16 @@ app.get('/ping-wordle', async (req, res) => {
   });
 
   const data = await response.json();
-
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.json(data);
 });
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
